000379ba0fe265f723e7e4e39c6cfe4c21f320c4
ruhi-jaiswal <108694149+ruhi-jaiswal@users.noreply.github.com>
2026-02-14 12:28:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dd5gfsuju/video/upload/v1771087687/marcarvy-rhythm-in-your-life-217576_ecdeog.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dd5gfsuju/video/upload/v1771090086/jakob_welik-minuit-a-montmartre-411961_ypl4hq.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
